26ae43b0ff3904937d54513a1605599d22bfa63c
MirkoFarina <MirkoFarina@outlook.com>
2022-11-14 15:03:25 +0100

    add ex

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from "./components/AppTitle.vue"
+export default {
+    components: {
+        AppTitle
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <AppTitle/>
+    </div>
+</template>
+
+<style>
+    .container {
+        width: 50%;
+        margin: 50px auto;
+        text-align: center;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <h1>La mia prima App con Vite !!</h1>
+</template>
+
+<style scoped>
+    h1 {
+        color:brown;
+    }
+</style>
